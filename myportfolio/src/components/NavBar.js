48b877d4ae374eb32f7c496df700aab0b28de793
zacharybaca <baca.zach@icloud.com>
2020-02-25 16:22:39 -0600

    created navbar component

---
+++ myportfolio/src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <h1>Zachary Baca</h1>
+    </div>
+  );
+}
+
+export default NavBar;
