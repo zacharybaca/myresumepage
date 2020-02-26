38f43553980408cf48da1619ff86fe48b75e10b3
zacharybaca <baca.zach@icloud.com>
2020-02-26 17:03:28 -0600

    added some changes to navbar, imported logo

--- myportfolio/src/components/NavBar.js
+++ myportfolio/src/components/NavBar.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Link, BrowserRouter as Router } from 'react-router-dom';
+import logo from '../assets/default.svg';
 import './NavBar.css';
 
 function NavBar() {
   return (
     <div className="navBarContainer">
       <div id="logo">
-        <h1>Zachary Baca</h1>
-        <div className="navbar">
-          <Router>
-            <Link to="#">Home</Link>
-            <Link to="#">Projects</Link>
-            <Link to="#">About Me</Link>
-          </Router>
-        </div>
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="navbar">
+        <Router>
+          <Link to="#">Home</Link>
+          <Link to="#">Projects</Link>
+          <Link to="#">About Me</Link>
+        </Router>
       </div>
     </div>
   );
