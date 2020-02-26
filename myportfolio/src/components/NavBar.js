f06ab2ce20a561370489344f7821442ca965125d
zacharybaca <baca.zach@icloud.com>
2020-02-26 14:14:17 -0600

    started to create the nav bar

--- myportfolio/src/components/NavBar.js
+++ myportfolio/src/components/NavBar.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
 import './NavBar.css';
 
 function NavBar() {
   return (
-    <div className="navbar">
-      <h1>Zachary Baca</h1>
+    <div className="navBarContainer">
+      <div id="logo">
+        <h1>Zachary Baca</h1>
+        <div className="navbar">
+          <Router>
+            <Link to="#">Home</Link>
+            <Link to="#">Projects</Link>
+            <Link to="#">About Me</Link>
+          </Router>
+        </div>
+      </div>
     </div>
   );
 }
